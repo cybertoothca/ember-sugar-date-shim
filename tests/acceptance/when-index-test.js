10973335677cc1f05edc8e6c80b63dc9592de2d7
Dan Nelson <dnelson@cybertooth.io>
2018-01-07 19:52:12 -0700

    Fixing a unit test whose timezone wasn't taken into consideration in the assertion.

--- tests/acceptance/when-index-test.js
+++ tests/acceptance/when-index-test.js
@@ -1,5 +1,6 @@
 import {test} from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import Sugar from 'ember-sugar-date';
 
 moduleForAcceptance('Acceptance | when index');
 
@@ -8,6 +9,7 @@ test('visiting /', function (assert) {
 
   andThen(function () {
     assert.equal(currentURL(), '/');
-    assert.equal(find('.js-test-sugar-date-full-en-ca').text(), 'Tuesday, 11 September, 2001 6:46');
+    assert.equal(find('.js-test-sugar-date-full-en-ca').text(),
+      Sugar.Date.full(new Date(Date.UTC(2001, 8, 11, 12, 46, 40)), 'en-CA'));
   });
 });
