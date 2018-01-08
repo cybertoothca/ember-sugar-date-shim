fcf96b452abc129a2426084ed51ad40a5ea6c584
Dan Nelson <dnelson@cybertooth.io>
2018-01-07 19:22:47 -0700

    Fix circle config.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,34 @@
 /* eslint-env node */
 module.exports = {
+  useYarn: true,
   scenarios: [
+    {
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-4'
+        },
+        resolutions: {
+          'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
     {
       name: 'ember-lts-2.8',
       bower: {
